89c71a04e2a27a6c75c346b8b95f2ee5d3a87139
Cassandra <cassandra.moore1516@gmail.com>
2025-06-26 20:37:24 -0500

    Fix code

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,24 @@
-import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import DotenvWebpackPlugin from 'dotenv-webpack';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import path from 'path';
 
 const prod = process.env.NODE_ENV === 'production';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default {
-  entry: './src/index.jsx',
+  entry: "./src/index.jsx",
   output: {
-    filename: prod ? 'bundle.[contenthash].js' : 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    filename: prod ? "bundle.[contenthash].js" : "bundle.js",
+    path: path.resolve("dist"),
+    publicPath: "/",
     clean: true,
   },
-  resolve: { extensions: ['.js', '.jsx'] },
-  devtool: prod ? 'source-map' : 'eval-cheap-module-source-map',
+  resolve: { extensions: [".js", ".jsx"] },
+  devtool: prod ? "source-map" : "eval-cheap-module-source-map",
   devServer: {
     hot: true,
     historyApiFallback: true,
     port: 3000,
-    static: './dist',
   },
   module: {
     rules: [
@@ -31,32 +26,29 @@ export default {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
             cacheDirectory: true,
-            plugins: [!prod && 'react-refresh/babel'].filter(Boolean),
+            // Only use react-refresh in dev
+            plugins: [!prod && "react-refresh/babel"].filter(Boolean),
           },
         },
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.scss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset',
+        type: "asset",
       },
     ],
   },
   plugins: [
     new DotenvWebpackPlugin(),
-    new HtmlWebpackPlugin({ template: 'public/index.html' }),
+    new HtmlWebpackPlugin({ template: "public/index.html" }),
     !prod && new ReactRefreshWebpackPlugin(),
   ].filter(Boolean),
-  mode: prod ? 'production' : 'development',
+  mode: prod ? "production" : "development",
   performance: { hints: false },
 };
